bce57a49e7170f2e351abf6740d471d31fd8ae6c
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-07-14 19:55:47 +0200

    merge dev et bdd

--- back/routes/bestiaire.js
+++ back/routes/bestiaire.js
@@ -4,7 +4,7 @@ const connection = require('../bdd/bdd.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM bestiaires', function(err, result, fields){
+  connection.query('SELECT * FROM bestiaire', function(err, result, fields){
     if (err){
       res.status(500).json({
         "status code" : 500,
