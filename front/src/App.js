a8db289e16eebf39d335d275bff6e665d1db45a9
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-07-15 13:35:57 +0200

    barre left page acceuil OK

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      bestiaire : []
-     }
+    this.state = {
+      bestiaire: [],
+    };
   }
   componentDidMount() {
-    console.log('je rentre dans le fetch')
+    console.log('je rentre dans le fetch');
     fetch('/bestiaire')
       .then(res => res.json())
-      .then(result => this.setState({ bestiaire : result }));
-    
-    }
+      .then(result => this.setState({ bestiaire: result }));
+  }
 
   render() {
-    const bestiaire = this.state.bestiaire
-    console.log(bestiaire)
+    const bestiaire = this.state.bestiaire;
+    console.log(bestiaire);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid fluid>
+          <Row className="titleNav">
+            <Col xs={1} className="col1">
+              <div className="title">
+                <div className="word1">
+                  <p> A </p>
+                  <p> R</p>
+                  <p> T </p>
+                  <p> I </p>
+                  <p> S </p>
+                  <p> A </p>
+                  <p> N </p>
+                  <p> A </p>
+                  <p> T </p>
+                </div>
+                <div className="word2">
+                  <p> D'</p>
+                  <p> A </p>
+                  <p> R </p>
+                  <p> T </p>
+                </div>
+              </div>
+            </Col>
+            <Col fluid xs={11} className="col10">
+              <div className="acceuil"> Ici le reste de la page d'acceuil</div>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
 }
-
 
 export default App;
