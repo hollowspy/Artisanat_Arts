1df7c57939481f34aadeb9fb3a18f2b615be97ca
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-07-24 18:58:42 +0200

    mise en place modal vegetal

--- front/src/Components/Vegetal.js
+++ front/src/Components/Vegetal.js
@@ -1,49 +1,104 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import {
+  Grid,
+  Row,
+  Col,
+  Popover,
+  Tooltip,
+  Modal,
+  Button,
+} from 'react-bootstrap';
 import _ from 'lodash';
 import '../style/Vegetal.css';
 
 class Vegetal extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      vegetal : {},
-     }
+    this.state = {
+      vegetal: {},
+      source: '',
+      show: false,
+    
+
+    };
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   componentDidMount() {
     console.log('je rentre dans le fetch de vegetal');
     fetch('/vegetal')
       .then(res => res.json())
-      .then(result => this.setState({ vegetal: result }));
+      .then(result =>
+        this.setState({
+          vegetal: result,
+        })
+      );
   }
 
-  render() { 
-    const {vegetal} = this.state
-    console.log('page vegetal', vegetal)
-    return ( 
-      <div>
-      <h1> Le vegetal </h1>
-      <Grid>
-      <Row>
-      {!_.isEmpty(vegetal) && 
-        vegetal.map(tableau => (
-          <div>
-            <Col xs={12} sm={4}>
-            <h2>{tableau.name}</h2>
-            <img src={tableau.photo_principale} alt='tableau' className="photo_tableau"/>
-            </Col>
-          </div>
+  handleClose() {
+    this.setState({ show: false });
+  }
 
-        ))}
-      
-      </Row>
-      </Grid>
-     
-      
+  handleShow(e, index) {
+    this.setState({ show: index });
+  }
+
+  render() {
+    const { vegetal } = this.state;
+    console.log('page vegetal', vegetal);
+
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    );
+    const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
+
+    return (
+      <div>
+        <h1> Le vegetal </h1>
+        <Grid>
+          <Row>
+            {!_.isEmpty(vegetal) &&
+              vegetal.map((tableau, index) => (
+                <div key={tableau.id}>
+                  <Col xs={12} sm={4}>
+                    <div>
+                      <h2>{tableau.name}</h2>
+                      <img
+                        src={tableau.photo_principale}
+                        alt="tableau"
+                        className="photo_tableau"
+                        onClick={e=>this.handleShow(e,index)}
+                      />
+                      <p>{tableau.name}</p>
+                      <Modal
+                        id={tableau.id}
+                        show={this.state.show === index}
+                        onHide={this.handleClose}
+                        dialogClassName="custom-modal"
+                      >
+                        <Modal.Header closeButton>
+                          <Modal.Title>Modal heading</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                          <h4>phoo dans modal ! </h4>
+                          <img src={tableau.photo_principale} className="photo_modal" />
+                        </Modal.Body>
+                        <Modal.Footer>
+                          <Button onClick={this.handleClose}>Close</Button>
+                        </Modal.Footer>
+                      </Modal>
+                    </div>
+                  </Col>
+                </div>
+              ))}
+          </Row>
+        </Grid>
       </div>
-     );
+    );
   }
 }
- 
+
 export default Vegetal;
