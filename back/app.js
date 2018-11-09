3835c559ba36551329d8fd5edeb982eb587f91f5
hollowspy <julien.niedzwiecki@neuf.fr>
2018-11-09 11:29:17 +0100

    carrousel OK

--- back/app.js
+++ back/app.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", true)
--- front/src/Components/Home/carousel.js
+++ front/src/Components/Home/carousel.js
@@ -1,15 +1,61 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Carousel, Container, Row, Col} from 'react-bootstrap';
+import _ from 'lodash';
+import './carousel.css'
 
-class Carousel extends Component {
+class CarouselHome extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {
+            srcPhoto: []
+
+        };
     }
+
+    componentDidMount() {
+        console.log('je rentre dans le fetch')
+        fetch('http://localhost:4000/api/carousel', {
+                method: 'POST',
+                headers: new Headers({'Content-Type': 'application/json'})
+            })
+            .then(res => res.json())
+            .then(result => this.setState({srcPhoto: result}))
+        console.log('sortie du fetch', this.state.srcPhoto)
+    }
+
     render() {
+        const {srcPhoto} = this.state
+        console.log('test fetch', {srcPhoto})
+        // console.log('test source', srcPhoto[0].source)
         return (
-            <div>Carousel Work's</div>
+            <div>
+                <Container fluid>
+                    <Row>
+                        <Col xs={12} className="containerCarousel">
+                        {!_.isEmpty(srcPhoto) && <Carousel>
+                            <Carousel.Item>
+                                <img className="photoCarousel" src={srcPhoto[0].source} alt="First slide" />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="photoCarousel" src={srcPhoto[1].source} alt="Second slide"/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="photoCarousel" src={srcPhoto[2].source} alt="Third slide"/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="photoCarousel" src={srcPhoto[3].source} alt="Fourth slide"/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="photoCarousel" src={srcPhoto[4].source} alt="Fifth slide"/>
+                            </Carousel.Item>
+                        </Carousel>
+                        }
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
 
-export default Carousel;
+export default CarouselHome;
