74174de728abdb9fa8a993338fe6d8e38aa3ddf0
hollowspy <julien.niedzwiecki@neuf.fr>
2018-11-16 16:10:33 +0100

    style CSS page Bestiaire OK

--- front/src/Containers/Bestiaire.js
+++ front/src/Containers/Bestiaire.js
@@ -1,29 +1,51 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchDatas } from '../Actions/FetchData'
-
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchDatas} from '../Actions/FetchData';
+import _ from 'lodash';
+import {Container, Row, Col} from 'react-bootstrap';
+import './Bestiaire.css'
 
 const MapStateToProps = state => {
-    return { Bestiaire : state.Bestiaire}
+    return {Bestiaire: state.Bestiaire}
 }
-
 
 class Bestiaire extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {};
     }
 
-    componentDidMount(){
-        this.props.dispatch(fetchDatas())
+    componentDidMount() {
+        this
+            .props
+            .dispatch(fetchDatas())
     }
 
     render() {
-    const { Bestiaire } = this.props
-    console.log('mon bestiaire from Redux', Bestiaire)
-     return (
-            <div>Bestiaire from Reducers is working !!</div>
-        );
+        const {Bestiaire} = this.props
+        console.log('mon bestiaire from Redux', Bestiaire.datas)
+        return (
+            <div>
+                <Container className="containerData">
+                    <h4>Le Bestiaire</h4>
+                    <Row>
+                        {!_.isEmpty(Bestiaire) && 
+                            Bestiaire.datas.map(oeuvre => 
+                            <Col sm={6} className="containerCard">
+                                <figure class="snip1165">
+                                    <img src={oeuvre.Aphoto_principale} alt="sample62" className="photoPrincipale"/>
+                                <figcaption>
+                                    <button className="btn btn-default">Plus de détails</button>
+                                </figcaption>
+                                </figure>
+
+                            </Col>
+                        )}
+                    </Row>
+                </Container>
+            </div>
+
+        )
     }
 }
 
